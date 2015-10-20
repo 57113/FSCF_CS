734582a21e0247cecbabec8ae207d93281ea9bbe
SHAYB57113 <sbarkham.57113@farnborough.ac.uk>
2015-10-20 10:14:23 +0100

    IqRejection0.0.1

--- src/com/as/cs/excersises/IQrejection.java
+++ src/com/as/cs/excersises/IQrejection.java
@@ -1,10 +1,42 @@
 package com.as.cs.excersises;
 
+import java.util.Scanner;
+
 public class IQrejection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+iq();
 	}
-
+public static void iq(){
+	int iq = 0;
+	int errors = 0;
+	@SuppressWarnings("resource")
+	Scanner sc=new Scanner(System.in);
+	System.out.print("What is your IQ");
+	iq = sc.nextInt();
+	while(iq <200){
+		System.out.println("What an IQ! " + iq);
+		if (iq<50);
+		System.out.println("To Low IQ");
+		errors = errors + 1;
+		if(iq >200);
+		System.out.println("Your not fooling anyone with that IQ");
+		errors = errors + 1;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
