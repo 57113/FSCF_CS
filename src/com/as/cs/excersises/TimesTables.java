c37b30301688f515d64e0295ba87371680d9ad9f
SHAYB57113 <sbarkham.57113@farnborough.ac.uk>
2015-11-03 10:27:57 +0000

    Times Tables 0.0.1

---
+++ src/com/as/cs/excersises/TimesTables.java
@@ -0,0 +1,16 @@
+package com.as.cs.excersises;
+import java.util.Scanner;
+public class TimesTables {
+
+	public static void main(String[] args) {
+		timeTable();
+
+	}
+public static void timeTable(){
+	System.out.print("Please Enter A Number Table: ");
+	@SuppressWarnings("resource")
+	Scanner sc=new Scanner(System.in);
+	int table = sc.nextInt();
+for ()
+}
+}
