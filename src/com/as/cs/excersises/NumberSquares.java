478be47e2cae2b86775610de3ed9fc040e8e1138
SHAYB57113 <sbarkham.57113@farnborough.ac.uk>
2015-11-12 12:05:18 +0000

    12/11/2015 12:05

---
+++ src/com/as/cs/excersises/NumberSquares.java
@@ -0,0 +1,19 @@
+package com.as.cs.excersises;
+import java.util.Scanner;
+public class NumberSquares {
+	public static void main(String[] args) {
+		int[] userList = {};
+	@SuppressWarnings("resource")
+	Scanner sc=new Scanner(System.in);
+	for (int a = 0; a == 9; a++){
+		System.out.print("Please enter a number");
+		userList[a] = sc.nextInt();
+	}
+		printArray(userList);
+	}	
+public static void printArray(int[] array){
+	for(int i = 0; i < array.length;i++){
+	System.out.println(array[i] + " ");	
+	}
+}
+}
